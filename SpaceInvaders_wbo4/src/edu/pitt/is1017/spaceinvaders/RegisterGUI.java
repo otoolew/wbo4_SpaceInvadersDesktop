1b2528aeec13faa4e60d1060c5287b22219352b0
Bill O'Toole <otoolew@gmail.com>
2016-01-21 20:25:19 -0500

    first commit

---
+++ SpaceInvaders_wbo4/src/edu/pitt/is1017/spaceinvaders/RegisterGUI.java
@@ -0,0 +1,7 @@
+package edu.pitt.is1017.spaceinvaders;
+
+import javax.swing.JFrame;
+
+public class RegisterGUI extends JFrame{
+
+}
---
+++ SpaceInvaders_wbo4/src/edu/pitt/is1017/spaceinvaders/Tester.java
@@ -0,0 +1,14 @@
+package edu.pitt.is1017.spaceinvaders;
+
+public class Tester {
+
+	public static void main(String[] args) {
+		DbUtilities db = new DbUtilities();
+		
+		String firstName = "Harry", lastName = "Black", email = "blackdog7@gmail.com";
+		
+		
+		
+	}
+
+}
